8ab5b1fc7e6466f9e949585b6cd5dcdc6db71673
Khaldoun Halawani <kldoon@hebron.edu>
2025-10-08 09:16:13 +0300

    [8-10] Navigation, passing params

--- doctor-app/screens/manage-patients/manage-patients.jsx
+++ doctor-app/screens/manage-patients/manage-patients.jsx
@@ -1,12 +1,34 @@
-import { View, Text } from "react-native"
+import { View, Text, ScrollView, TouchableOpacity } from "react-native"
+import { PATIENTS } from "../../data/patients";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useNavigation } from "@react-navigation/native";
 
 const ManagePatients = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>
-        Manage Patients
-      </Text>
-    </View>
+    <ScrollView>
+      {
+        PATIENTS.map(item => (
+          <View key={item.email}>
+            <Text>
+              {item.firstName} {item.lastName}
+            </Text>
+            <Text>
+              {item.dob}
+            </Text>
+            <Text>
+              {item.gender === 1 ? "Male" : "Female"}
+            </Text>
+            <TouchableOpacity onPress={() => {
+              navigation.navigate("ViewPatient", { email: item.email });
+            }}>
+              <MaterialCommunityIcons name="arrow-right-bold-circle-outline" size={24} color="blue" />
+            </TouchableOpacity>
+          </View>
+        ))
+      }
+    </ScrollView>
   )
 }
 
--- doctor-app/screens/view-patient/view-patient.jsx
+++ doctor-app/screens/view-patient/view-patient.jsx
@@ -1,10 +1,39 @@
+import { useRoute } from "@react-navigation/native";
+import { useEffect, useState } from "react";
 import { View, Text } from "react-native"
+import { PATIENTS } from "../../data/patients";
 
 const ViewPatient = () => {
+  const router = useRoute();
+  const email = router.params["email"] || "";
+  const [currentPatient, setCurrentPatient] = useState({});
+
+  useEffect(() => {
+    const patient = PATIENTS.find(p => p.email === email);
+    if (patient) {
+      setCurrentPatient(patient);
+    }
+  }, [email]);
+
   return (
     <View>
-      <Text>
+      <Text style={{ fontSize: 25, fontWeight: 700 }}>
         ViewPatient
+      </Text>
+      <Text>
+        {currentPatient.firstName} {currentPatient.lastName}
+      </Text>
+      <Text>
+        {currentPatient.gender === 1 ? "Male" : "Female"}
+      </Text>
+      <Text>
+        {currentPatient.email}
+      </Text>
+      <Text>
+        {currentPatient.dob}
+      </Text>
+      <Text>
+        {currentPatient.mobile}
       </Text>
     </View>
   )
