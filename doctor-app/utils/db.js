1c5cc8776e22c4ea14dbbf94de7c90d769d711b7
Khaldoun Halawani <kldoon@hebron.edu>
2025-10-29 08:58:10 +0200

    [29-10] Async Prog (then, catch)

--- doctor-app/utils/db.js
+++ doctor-app/utils/db.js
@@ -3,8 +3,8 @@ import { SQLiteDatabase } from 'expo-sqlite';
 /**
  * @param {SQLiteDatabase} db 
  */
-export async function initDatabase(db) {
-  await db.execAsync(`
+export function initDatabase(db) {
+  db.execAsync(`
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
       firstName TEXT NOT NULL,
@@ -15,22 +15,77 @@ export async function initDatabase(db) {
       gender INTEGER NOT NULL DEFAULT 0,
       dob TEXT
     );
-  `);
+  `).then().catch(err => {
+    console.log("Error creating table!", err);
+  });;
 
-  const count = await db.getFirstAsync('SELECT COUNT(*) as count FROM users');
-  if (count.count === 0) {
-    const sampleDob = new Date('1990-01-01').toISOString();
-    await db.runAsync(`
+  // Sync (blocking) code
+  // const x = Math.log10(500);
+  // console.log(x);
+
+  db.getFirstAsync('SELECT COUNT(*) as count FROM users')
+    .then((res) => {
+      if (res.count < 2) {
+        const sampleDob = new Date('1990-01-01').toISOString();
+
+        db.runAsync(`
       INSERT INTO users (firstName, lastName, email, mobile, password, gender, dob) 
       VALUES ($firstName, $lastName, $email, $mobile, $password, $gender, $dob)
     `, {
-      $firstName: 'Ahmad',
-      $lastName: 'Saeed',
-      $email: 'ahmad@example.com',
-      $mobile: '123-456-7890',
-      $password: '123456',
-      $gender: 0,
-      $dob: sampleDob
+          $firstName: 'Ahmad',
+          $lastName: 'Saeed',
+          $email: 'ahmad2@example.com',
+          $mobile: '123-456-7890',
+          $password: '123456',
+          $gender: 0,
+          $dob: sampleDob
+        }).then(res => {
+          console.log(res);
+          if (res.changes != 0) {
+            //success
+          }
+        }).catch(err => {
+          console.log("Error inserting first item!", err);
+        });
+      }
+    })
+    .catch(err => {
+      console.log("Error inserting first item!", err);
     });
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // const count = await db.getFirstAsync('SELECT COUNT(*) as count FROM users');
+
+  // if (count.count === 0) {
+  //   const sampleDob = new Date('1990-01-01').toISOString();
+  //   await db.runAsync(`
+  //     INSERT INTO users (firstName, lastName, email, mobile, password, gender, dob) 
+  //     VALUES ($firstName, $lastName, $email, $mobile, $password, $gender, $dob)
+  //   `, {
+  //     $firstName: 'Ahmad',
+  //     $lastName: 'Saeed',
+  //     $email: 'ahmad@example.com',
+  //     $mobile: '123-456-7890',
+  //     $password: '123456',
+  //     $gender: 0,
+  //     $dob: sampleDob
+  //   });
+  // }
 }
